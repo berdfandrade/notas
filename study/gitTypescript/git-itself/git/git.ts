54314f3f5a9c7ffb73407abefefa7b32f953779c
Bernardo Andrade <berdfandrade@gmail.com>
2023-10-15 21:56:47 -0300

    --

--- study/gitTypescript/git-itself/git/git.ts
+++ study/gitTypescript/git-itself/git/git.ts
@@ -11,3 +11,14 @@ export class Git implements GitI {
 }
 
 
+/*
+    import { GitI } from "./git-interface": Este trecho de código está importando a interface GitI do arquivo git-interface. Interfaces em TypeScript são usadas para definir a estrutura que as classes devem seguir. Neste caso, provavelmente a interface GitI contém a definição das funções e propriedades que as classes que implementam essa interface devem ter.
+
+    export class Git implements GitI: Aqui, uma classe chamada Git é definida e ela implementa a interface GitI. Isso significa que a classe Git deve conter as funções e propriedades especificadas na interface GitI.
+
+    name: string: A classe Git possui uma propriedade chamada name, que é do tipo string. Esta propriedade provavelmente é usada para armazenar o nome de alguma entidade relacionada ao Git.
+
+    constructor(name: string): Este é o construtor da classe Git. Ele recebe um argumento name, que é uma string, e atribui esse valor à propriedade name da instância da classe.
+
+	Resumindo, esse código define uma classe chamada Git que implementa a interface GitI. Essa classe possui uma propriedade chamada name que pode ser definida no momento da criação da instância através do construtor. O objetivo específico dessa classe e sua interface dependerá do restante do código que não foi mostrado aqui.
+*/
